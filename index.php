24ddce355d786c4575c01ede22c013e60d107ecf
Isaac Campos <kakoo26i@gmail.com>
2017-02-21 03:19:04 +0000

    add hybridauth using composer

--- index.php
+++ index.php
@@ -1,4 +1,20 @@
-<?php error_reporting(E_ERROR); ?>
+<?php //error_reporting(E_ERROR); 
+    require 'vendor/autoload.php';
+
+    try{
+        $hybridauth = new Hybrid_Auth( 'config.php' );
+        
+        $twitter = $hybridauth->authenticate( "Facebook" );
+        
+        $user_profile = $twitter->getUserProfile();
+        
+        echo "Hi there! " . $user_profile->displayName;
+    }
+    catch( Exception $e ){
+        echo "Ooophs, we got an error: " . $e->getMessage();
+    }
+?>
+
 <?php include "header.php"; ?>
 
     <?php include "page-templates/login-page.php"; ?>
